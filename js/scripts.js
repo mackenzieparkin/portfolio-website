84ee7892aeaad40152cd5edde612fd2c88c8ffaf
mackenzieparkin <108953495+mackenzieparkin@users.noreply.github.com>
2022-08-10 11:27:40 -0600

    Hamburger Icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 console.log("Hi! Welcome to My Portfolio Site.")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
